2285544d7052d4a7c09796972cc2825fef83b231
pyabzz <>
2022-06-04 23:20:40 +0100

    Develop Order further

--- Core/Model/Order.cs
+++ Core/Model/Order.cs
@@ -16,16 +16,18 @@ namespace Core
     public class Order : IOrder
     {
         public int CustomerId { get; private set; }
-        public IEnumerable<IOrderItem> Items => items;
+        public IEnumerable<IOrderItem> Items => items.Values.ToArray();
         public bool Add(IOrderItem item)
         {
-            items.Add(item);
+            if (items.ContainsKey(item.ProductId))
+                return false;
+            items.Add(item.ProductId, item);
             return true;
         }
         public decimal Amount => Items.Sum(x => x.Amount);
         public bool Submit() => throw new NotImplementedException();
 
-        private List<IOrderItem> items = new List<IOrderItem>();
+        private Dictionary<int, IOrderItem> items = new Dictionary<int, IOrderItem>();
 
         private Order() { }
         public static Order Create(int customerId)
--- Test/Test/OrderTest.cs
+++ Test/Test/OrderTest.cs
@@ -9,55 +9,93 @@ namespace Test
     {
         Random rng = new Random();
 
+        private Order GetSut()
+        {
+            var id = rng.Next();
+            return Order.Create(id);
+        }
+
         [Fact]
         public void Create_InitialisesState()
         {
             var id = rng.Next();
-            var instance = Order.Create(id);
-            Assert.Equal(id, instance.CustomerId);
+            var sut = Order.Create(id);
+            Assert.Equal(id, sut.CustomerId);
         }
 
         [Fact]
         public void Add_AppendsToItems()
         {
-            var id = rng.Next();
-            var instance = Order.Create(id);
+            var sut = GetSut();
 
             var itemMocker0 = new Mock<IOrderItem>();
+            itemMocker0.SetupGet(x => x.ProductId).Returns(rng.Next());
             var itemMock0 = itemMocker0.Object;
-            instance.Add(itemMock0);
+            sut.Add(itemMock0);
 
             var itemMocker1 = new Mock<IOrderItem>();
+            itemMocker1.SetupGet(x => x.ProductId).Returns(rng.Next());
             var itemMock1 = itemMocker1.Object;
-            instance.Add(itemMock1);
+            sut.Add(itemMock1);
 
             Assert.Collection(
-                instance.Items,
+                sut.Items,
                 x => Assert.Same(itemMock0, x),
                 x => Assert.Same(itemMock1, x)
                 );
         }
 
         [Fact]
+        public void Add_ReturnsTrue_IfAppendsSuccessfully()
+        {
+            var sut = GetSut();
+
+            var itemMocker0 = new Mock<IOrderItem>();
+            var itemMock0 = itemMocker0.Object;
+            var result = sut.Add(itemMock0);
+
+            Assert.True(result);
+            Assert.Collection(sut.Items, x => Assert.Same(itemMock0, x));
+        }
+
+        [Fact]
+        public void Add_ReturnsFalse_IfDuplicateProduct()
+        {
+            var sut = GetSut();
+            var productId = rng.Next();
+
+            var itemMocker0 = new Mock<IOrderItem>();
+            itemMocker0.SetupGet(x => x.ProductId).Returns(productId);
+            var itemMock0 = itemMocker0.Object;
+            Assert.True(sut.Add(itemMock0));
+
+            var itemMocker1 = new Mock<IOrderItem>();
+            itemMocker1.SetupGet(x => x.ProductId).Returns(productId);
+            var itemMock1 = itemMocker1.Object;
+            Assert.False(sut.Add(itemMock1));
+        }
+
+        [Fact]
         public void Amount_SumsAmountsOfItems()
         {
-            var id = rng.Next();
-            var instance = Order.Create(id);
+            var sut = GetSut();
 
             var amount0 = Utils.GetRandom(0, 200);
             var itemMocker0 = new Mock<IOrderItem>();
+            itemMocker0.SetupGet(x => x.ProductId).Returns(rng.Next());
             itemMocker0.SetupGet(x => x.Amount).Returns(amount0);
             var itemMock0 = itemMocker0.Object;
-            instance.Add(itemMock0);
+            sut.Add(itemMock0);
 
             var amount1 = Utils.GetRandom(0, 200);
             var itemMocker1 = new Mock<IOrderItem>();
+            itemMocker1.SetupGet(x => x.ProductId).Returns(rng.Next());
             itemMocker1.SetupGet(x => x.Amount).Returns(amount1);
             var itemMock1 = itemMocker1.Object;
-            instance.Add(itemMock1);
+            sut.Add(itemMock1);
 
             var total = amount0 + amount1;
-            Assert.Equal(total, instance.Amount);
+            Assert.Equal(total, sut.Amount);
         }
     }
 }
