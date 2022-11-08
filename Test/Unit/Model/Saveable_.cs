d8e2126f83145e8acd0bd8b47d6b8fc4f5eec8c8
pyabzz <>
2022-11-08 00:31:13 +0000

    Add unit test for Saveable

---
+++ Test/Unit/Model/Saveable_.cs
@@ -0,0 +1,31 @@
+namespace Unit;
+
+public class Saveable_
+{
+    private class SUT : Saveable
+    {
+        public SUT() { }
+        public SUT(int id) => Id = id;
+    }
+
+    [Fact]
+    public void Id_Defaults_ToNull()
+    {
+        SUT sut = new();
+        Assert.Null(sut.Id);
+    }
+
+    [Fact]
+    public void IsNew_WhenIdIsNull_GetsTrue()
+    {
+        SUT sut = new();
+        Assert.True(sut.IsNew);
+    }
+
+    [Fact]
+    public void IsNew_WhenIdHasValue_GetsFalse()
+    {
+        SUT sut = new(Randomiser.AnId);
+        Assert.False(sut.IsNew);
+    }
+}
