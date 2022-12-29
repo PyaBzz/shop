0167b8adfa9cd0c60658d66fc47f92fab1a9e830
pyabzz <>
2022-12-29 21:27:02 +0000

    Use Baz nuget package

--- Test/Unit/Model/Product_ReleaseDate.cs
+++ Test/Unit/Model/Product_ReleaseDate.cs
@@ -10,13 +10,6 @@ public class Product_ReleaseDate : Product_
     }
 
     [Fact]
-    public void WhenReleasedInvalid_GivesTheRightValidationMessage()
-    {
-        Product sut = new Product(dummyName, Randomiser.APrice, tomorrow);
-        ModelStateTester.Do(sut, false, Message.invalid_release_date);
-    }
-
-    [Fact]
     public void WhenReleasedYesterday_IsValid()
     {
         Product sut = new Product(dummyName, Randomiser.APrice, yesterday);
