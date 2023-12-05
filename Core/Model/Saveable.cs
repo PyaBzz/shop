e374bed6618ff8f548df9c95584a1ea280ab2add
pyabzz <>
2023-12-05 01:00:43 +0000

    Add ISaveable

--- Core/Model/Saveable.cs
+++ Core/Model/Saveable.cs
@@ -1,6 +1,13 @@
 namespace Core;
 
-public abstract class Saveable : IValidatable
+public interface ISaveable
+{
+    public int? Id { get; }
+    public bool IsNew { get; }
+    public abstract bool IsValid { get; }
+}
+
+public abstract class Saveable : ISaveable, IValidatable
 {
     public int? Id { get; protected set; }
     public bool IsNew => Id is null;
